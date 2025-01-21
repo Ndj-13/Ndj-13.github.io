c433c77b5bfda0af2a5a94ca5e3ef50528dbe20d
Ndj-13 <nerea.dzjrc@gmail.com>
2025-01-21 11:30:00 +0100

    CV Update

--- js/nereascripts.js
+++ js/nereascripts.js
@@ -1,6 +1,6 @@
 function playAnimation(avatarId){
     const avatar=document.getElementById(avatarId);
-    //avatar.pause();
+    //avatar.play();
     avatar.addEventListener('mouseenter',()=>{
         avatar.play().catch(error => console.error("Error al reproducir la animacion", error));
     });
